b3c82f2c66869f5bd8b04cce9f52340ada4349de
diego <diegopancho87@gmail.com>
2017-09-10 23:36:02 -0500

    uno

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { AngularFireDatabase, FirebaseListObservable } from 'angularfire2/database';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,119 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'app';  
+   my_notes:any;
+   my_notes_offline = [];
+
+constructor(public afDB: AngularFireDatabase) {
+    if(navigator.onLine){
+     this.getNotes()
+      .subscribe(
+        notas =>{
+         this.my_notes = notas;
+          localStorage.setItem('my_notes', JSON.stringify(this.my_notes));
+       }
+     );
+    }else{
+      this.my_notes = JSON.parse(localStorage.getItem('my_notes'));
+    }
+     setInterval(()=>{
+       if(navigator.onLine){
+         this.syncronize();
+       }else{
+         console.log('Offline!');
+       }
+     }, 5000);
+   }
+   //listando las notas
+   getNotes(){
+     return this.afDB.list('/notas');
+   }
+
+removeNote(){
+  if(navigator.onLine){
+    this.afDB.database.ref('notas/' + this.note.id).remove();
+  }else{
+      this.my_notes.forEach((nota, i) => {
+        if(nota.id == this.note.id){
+          this.my_notes.splice(i, 1);
+        }
+      });
+      this.my_notes_offline.push({
+          id: this.note.id,
+          action: 'remove'
+        });
+    }
+    this.show_form = false;
+    this.note = {id:null, title:null, description:null};
+    localStorage.setItem('my_notes', JSON.stringify(this.my_notes));  
+   }
+note = {id:null, title:null, description:null};
+show_form = false;
+editing = false;
+  addNote(){
+   this.editing = false;
+   this.show_form = true;
+   this.note = {id:null, title:null, description:null}; 
+  }
+  viewNote(note){
+   this.editing = true;
+   this.note = note;
+   this.show_form = true;
+  }
+  cancel(){
+   this.show_form = false;
+  } 
+createNote(){
+  if(this.editing){
+    if(navigator.onLine){
+      this.afDB.database.ref('notas/' + this.note.id).set(this.note);
+    }else{
+      this.my_notes.forEach((nota) => {
+        if(nota.id == this.note.id){
+          nota = this.note;
+        }
+      });
+      this.my_notes_offline.push({
+          id: this.note.id,
+          note: this.note,
+          action: 'edit'
+        });
+      }
+    }else{
+      this.note.id = Date.now();
+      if(navigator.onLine){
+        this.afDB.database.ref('notas/' + this.note.id).set(this.note);
+      }else{
+        this.my_notes.push(this.note);
+        this.my_notes_offline.push({
+          id: this.note.id,
+          note: this.note,
+          action: 'create'
+        });
+      }
+    }
+      this.show_form = false;
+      this.note = {id:null, title:null, description:null};
+      localStorage.setItem('my_notes', JSON.stringify(this.my_notes)); 
+} 
+ syncronize(){
+   this.my_notes_offline.forEach((record)=>{
+     switch(record.action){
+       case 'create':
+         this.afDB.database.ref('notas/' + record.note.id).set(record.note);
+         break;
+       case 'edit':
+         this.afDB.database.ref('notas/' + record.note.id).set(record.note);
+         break;
+       case 'remove':
+         this.afDB.database.ref('notas/' + record.id).remove();
+         break;
+       default:
+        console.log('Operacion no soportada');
+    }
+       this.my_notes_offline.shift();
+       console.log(this.my_notes_offline);
+   });
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,31 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { AngularFireAuthModule } from 'angularfire2/auth';
+
+export const firebaseConfig = {
+  apiKey: "AIzaSyAfvLYpIDeNIHVjRxOsxvJP5eqXpLlZL_o",
+  authDomain: "ng-notas-a1341.firebaseapp.com",
+  databaseURL: "https://ng-notas-a1341.firebaseio.com",
+  storageBucket: "ng-notas-a1341.appspot.com",
+  messagingSenderId: '1010548777486'
+};
+
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule,
+    AngularFireModule.initializeApp(firebaseConfig),
+    AngularFireDatabaseModule,
+    AngularFireAuthModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
